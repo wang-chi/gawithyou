1d71a0a5b498de08df95ab55b06791d09108ec69
wang-chi <h.wangchi.0970@gmail.com>
2019-10-09 23:04:05 +0800

    Day23 TSP

--- Day23-TSP.R
+++ Day23-TSP.R
@@ -1,17 +1,12 @@
 # Day23-GA resolved TSP
-citation("mcga")
-require("mcga")
+library(GA)
 
 # fitness vector
-A<-c('-1','25','25','10','35')
-B<-c('25','-1','25','35','25')
-C<-c('25','25','-1','30','15')
-D<-c('10','35','30','-1','20')
-E<-c('35','25','15','20','-1')
+A<-c(0,25,25,10,35)
+B<-c(25,0,25,35,25)
+C<-c(25,25,0,30,15)
+D<-c(10,35,30,0,20)
+E<-c(35,25,15,20,0)
 city<-rbind(A,B,C,D,E)
 colnames(city)<-c('A','B','C','D','E')
-
-#fitness function
-cost <- function(x){
-  
-}
+data<-as.matrix(city)
